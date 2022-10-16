d1cf99b3b33d748d9089cde64854b9c9b7492de5
anteyku <karkalvatas@yandex.ru>
2022-10-16 14:57:59 +0300

    update two

--- src/main.js
+++ src/main.js
@@ -29,7 +29,7 @@ import { SnackbarService, Vue3Snackbar } from "vue3-snackbar";
 import "vue3-snackbar/dist/style.css";
 
 
-//ВЕНЕРА
+//ДОБРЫЙ И КАБАН
 
 router.beforeEach(async (to, from, next) => {
   try{
